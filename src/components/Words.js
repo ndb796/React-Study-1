bd8d4d6ea685ad3f7e73e563ad4b2de894fb197d
ndb796 <ndb796@naver.com>
2019-04-17 22:58:44 +0900

    Get Word List & Show from Firebase

--- src/components/Words.js
+++ src/components/Words.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const databaseURL = "https://wordcloudtest1.firebaseio.com/";
 
 class Words extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            words: {}
+        };
+    }
+
+    _get() {
+        fetch(`${databaseURL}/words.json`).then(res => {
+            if(res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(words => this.setState({words: words}));
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.words != this.state.words
+    }
+
+    componentDidMount() {
+        this._get();
+    }
+
     render() {
         return (
-            <Card>
-                <CardContent>
-                    Words 페이지
-                </CardContent>
-            </Card>
+            <div>
+                {Object.keys(this.state.words).map(id => {
+                    const word = this.state.words[id];
+                    return (
+                        <Card key={id}>
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    가중치: {word.weight}
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {word.word}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    );
+                })}
+            </div>
+
         );
     }
 }
