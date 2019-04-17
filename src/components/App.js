6bd37ed044a12f90614de5276b625abc21bfa747
ndb796 <ndb796@naver.com>
2019-04-17 21:12:06 +0900

    Initialize React Project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello World</h3>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict'
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: ['./src/main.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, './src'),
+            loaders: 'babel-loader'
+        }]
+    },
+    plugins: [],
+    devServer: {
+        contentBase: './public',
+        host: 'localhost',
+        port: 8080
+    }
+}
